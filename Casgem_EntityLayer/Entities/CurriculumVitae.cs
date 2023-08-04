ace6728d3064f4be22b078a0af51be4bfc1c4735
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-04 11:59:22 +0300

    Add project files.

---
+++ Casgem_EntityLayer/Entities/CurriculumVitae.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem_EntityLayer.Entities
+{
+    //İş ilanı Entity'si
+    public class CurriculumVitae
+    {
+
+    }
+}
