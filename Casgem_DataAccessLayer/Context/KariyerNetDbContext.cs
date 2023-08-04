e31a4f14ca96fdec86f404606fe013b7ed3d2d87
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-04 15:45:20 +0300

    KariyerNetDbContext sınıfı yazıldı ve entity'ler tablolar ile eşlenmeye hazır.

---
+++ Casgem_DataAccessLayer/Context/KariyerNetDbContext.cs
@@ -0,0 +1,36 @@
+﻿using Casgem_EntityLayer.Entities.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casgem_DataAccessLayer.Context
+{
+    public class KariyerNetDbContext : IdentityDbContext<AppUser, AppRole, int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server = DESKTOP-13123BI; Initial Catalog = CasgemKariyerNetDb; Integrated Security = true;");
+        }
+
+        public DbSet<Company> Companies { get; set; } //Şirketler tablosu.
+        public DbSet<CurriculumVitae> CurriculumVitaes { get; set; } //CV tablosu.
+        public DbSet<Degree> Degrees { get; set; } //Ünvan tablosu.
+        public DbSet<EducationInformation> EducationInformations { get; set; }//Eğitim bilgileri.
+        public DbSet<Employer> Employers { get; set; }//İş verenler tablousu
+        //arama geçmişi.
+        //işveren iş ilanı arama geçmişi.
+        public DbSet<EmployerJobAdvertisementSearchHistory> EmployerJobAdvertisementSearchHistories { get; set; }
+        public DbSet<JobAdvertisement> JobAdvertisements { get; set; }//iş ilanları tablosu.
+        public DbSet<JobApplication> JobApplications { get; set; } //iş başvuruları tablosu.
+        public DbSet<JobSeeker> JobSeeker { get; set; } //iş arayanlar tablosu.
+        //arama geçmişi.
+        //iş arayanını yapmış olduğu aramalarının gelmişlerinin geçmişlerinin tutulduğu tablodur.
+        public DbSet<JobSeekerJobAdvertisementSearchHistory> JobSeekerJobAdvertisementSearchHistories { get; set; }
+        //iş ilanında yayınlanan iş verenler tarafından istenilen ve beklenen iş deneyimleri tablosudur.
+        public DbSet<WorkExperience> WorkExperiences { get; set; }
+    }
+}
--- Casgem_EntityLayer/Entities/Concrete/Degree.cs
+++ Casgem_EntityLayer/Entities/Concrete/Degree.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+//Ünvan!!!!
 namespace Casgem_EntityLayer.Entities.Concrete
 {
     //İş verenlerin ve iş arayanların ünvan Entity'si.
