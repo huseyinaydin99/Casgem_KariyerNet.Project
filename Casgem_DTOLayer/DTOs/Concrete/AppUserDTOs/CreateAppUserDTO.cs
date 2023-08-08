ce43c102cd4b2749a52ea0b35be45f88e04d28a2
huseyinaydin99 <huseyinaydin99@gmail.com>
2023-08-08 19:00:47 +0300

    AppUser DTO'su yazıldı.

--- Casgem_DTOLayer/DTOs/Concrete/AppUserDTOs/CreateAppUserDTO.cs
+++ Casgem_DTOLayer/DTOs/Concrete/AppUserDTOs/CreateAppUserDTO.cs
@@ -4,7 +4,20 @@ namespace Casgem_DTOLayer.DTOs.Concrete.AppUserDTOs
 {
     public class CreateAppUserDTO : IDTO
     {
-        public int Id { get; set; }
-        public string Password { get; set; }
+        //public int Id { get; set; }
+        public string NameSurname { get; set; }
+        public string UserName { get; set; }
+        public string NormalizedUserName { get; set; }
+        public string Email { get; set; }
+        public string NormalizedEmail { get; set; }
+        public bool EmailConfirmed { get; set; }
+        public string PasswordHash { get; set; }
+        public string SecurityStamp { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool PhoneNumberConfirmed { get; set; }
+        public bool TwoFactorEnabled { get; set; }
+        public DateTime LockoutEnd { get; set; }
+        public bool LockoutEnabled { get; set; }
+        public int AccessFailedCount { get; set; }
     }
 }
